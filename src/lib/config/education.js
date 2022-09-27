fad961ba4d5926b5005c953cbfe3aafbbfc8aab8
Alex So <alexskt123@Alexs-Air.lan>
2022-09-27 23:43:21 +0800

    add skills and honors

--- src/lib/config/education.js
+++ src/lib/config/education.js
@@ -1,14 +1,14 @@
 export const education = [
   {
     header: 'The Hong Kong University of Science & Technology',
-    listItems: ['Bachelor of Engineering in Computer Science']
+    listItems: ['Bachelor of Engineering in Computer Science', 'Second Class Honors (Upper Division)']
   },
   {
     header: 'The University of London',
-    listItems: ['Bachelor of Laws']
+    listItems: ['Bachelor of Laws', 'Second Class Honors (Upper Division)']
   },
   {
     header: 'The University of Hong Kong',
-    listItems: ['Postgraduate Certificate in Laws']
+    listItems: ['Postgraduate Certificate in Laws', 'Distinction in Drafting Commercial Agreements']
   }
 ];
